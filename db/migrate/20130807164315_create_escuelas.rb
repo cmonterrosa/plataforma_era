b2373e6fe1121572b69b87c2d868f0f66cfea896
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-08-12 15:51:40 -0500

    + Agregue nuevos campos a migracion de tabla escuelas

--- db/migrate/20130807164315_create_escuelas.rb
+++ db/migrate/20130807164315_create_escuelas.rb
@@ -5,9 +5,22 @@ class CreateEscuelas < ActiveRecord::Migration
         t.string :nombre, :limit => 20
         t.string :zona_escolar, :limit => 20
         t.string :sector, :limit => 10
+        t.string :telefono, :limit => 16
+        t.string :modalidad, :limit => 100
+        t.string :localidad, :limit => 120
+        t.string :municipio, :limit => 140
+        t.string :domicilio, :limit => 160
+        t.string :categoria, :limit => 140
+        t.string :correo_electronico, :limit => 90
+        t.string :nombre_director, :limit => 130
+        t.string :telefono_director, :limit => 100
+        t.integer :total_personal_docente
+        t.integer :total_personal_administrativo
+        t.integer :total_personal_apoyo
         t.integer :nivel_id
     end
     add_index :escuelas, :clave, :unique => true
+    add_index :escuelas, :nivel_id, :name => "nivel"
   end
 
   def self.down
