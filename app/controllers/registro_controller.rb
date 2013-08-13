45b834c9015d942f1a22799df1d0b905ab148bfb
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-08-12 20:17:29 -0500

    + Nuevos partials y metodo para mostrar datos de una escuela en particular

--- app/controllers/registro_controller.rb
+++ app/controllers/registro_controller.rb
@@ -2,13 +2,36 @@ class RegistroController < ApplicationController
   def index
     redirect_to :action => "new_or_edit"
   end
+
+  def index
+    @escuela = Escuela.new
+  end
   
   def new_or_edit
-    @escuela = Escuela.new
+    unless params[:escuela][:clave].size > 4 && @escuela = Escuela.find_by_clave(params[:escuela][:clave])
+      flash[:error] = "No existe escuela"
+      redirect_to :controller => "home"
+    end
   end
 
   def save
-    a=10
+    @escuela = Escuela.new(params[:escuela])
+    @proyecto = Proyecto.new(params[:proyecto])
+    @escuela.proyecto = @proyecto
+    if @escuela.save
+      flash[:notice] = "Registro guardado correctamente"
+      redirect_to :controller => "home"
+    else
+      flash[:error] = "No se puedo guardar, verifique el registro"
+      render :action => "new_or_edit"
+    end
+  end
+
+  def show_info_escuela
+    if params[:escuela_clave].size > 4
+      @escuela = Escuela.find_by_clave(params[:escuela_clave])
+      return render(:partial => 'list_by_user', :layout => false) if @escuela && request.xhr?
+    end
   end
 
 end
