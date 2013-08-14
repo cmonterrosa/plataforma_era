f0eb0ec42e11aa04f898a07aca0172535f11984b
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-08-13 19:36:19 -0500

    + Nuevo metodo para verificar si existe categoria en escuela

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def escuela_has_categoria?(escuela, categoria)
+    @escuela = Escuela.find(escuela)
+    @categoria = CategoriaEscuela.find(categoria)
+    (@escuela.categoria_escuela_id == @categoria.id) ? (return true) : (return false)
+  end
 end
