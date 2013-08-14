8a14ae979c0282b2a540731871302a2903ad4537
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-08-13 19:35:56 -0500

    + Metodos para guardar registro al mandar clave de ct

--- app/controllers/registro_controller.rb
+++ app/controllers/registro_controller.rb
@@ -12,15 +12,19 @@ class RegistroController < ApplicationController
       flash[:error] = "No existe escuela"
       redirect_to :controller => "home"
     end
+    @proyecto = @escuela.proyecto if @escuela
   end
 
   def save
-    @escuela = Escuela.find_by_clave(params[:escuela][:clave])
-    @escuela ||= Escuela.new(params[:escuela])
-    @proyecto = @escuela.proyecto
-    @proyecto ||= Proyecto.new(params[:proyecto])
+    @escuela = Escuela.find_by_clave(params[:clave]) if params[:clave]
+    @escuela ||= Escuela.new
+    @escuela.update_attributes(params[:escuela])
+    @escuela.categoria_escuela = CategoriaEscuela.find(params[:escuela][:categoria_escuela_id]) if params[:escuela][:categoria_escuela_id]
+    @proyecto = (@escuela.proyecto) ? @escuela.proyecto : Proyecto.new
+    @proyecto.update_attributes(params[:proyecto])
+    @proyecto.clave = @escuela.clave if @escuela.clave
     @escuela.proyecto = @proyecto
-    if @escuela.save
+    if @escuela.save && @proyecto.save
       flash[:notice] = "Registro guardado correctamente"
       redirect_to :controller => "home"
     else
