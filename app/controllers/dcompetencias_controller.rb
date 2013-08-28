ef87e96e3c8ce4170be799172813c6146b94977c
Rafael Hernandez Escobar <snakheertax@gmail.com>
2013-08-28 16:06:29 -0500

    Agrega metodo new_or_edit a dcompetencias

--- app/controllers/dcompetencias_controller.rb
+++ app/controllers/dcompetencias_controller.rb
@@ -1,6 +1,28 @@
 class DcompetenciasController < ApplicationController
-  def dcompetencias
+  layout 'era'
 
+  def index
+    new_or_edit
+    render :action => 'new_or_edit'
+  end
+
+  def new_or_edit
+    @reporte = Reporte.find(params[:id])
+    @tipo_reporte = TipoReporte.find(@reporte.tipo_reporte)
+    @escuela = Escuela.find(@tipo_reporte.escuela)
+    @dcomptencias = @reporte.dcompetencia || Dcompetencia.new
+ end
+
+  def save
+    begin
+      @dcomptencias = Dcompetencia.find(params[:id])
+      @dcomptencias.update_attributes!(params[:dcompetencia])
+        flash[:notice]= "El registro fué actualizado"
+        redirect_to new_or_edit_dcompetencia_path
+    rescue ActiveRecord::RecordInvalid => invalid
+      flash[:notice] = invalid
+      redirect_to tipo_reporte_path
+    end
   end
 
 end
