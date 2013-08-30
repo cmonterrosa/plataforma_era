84b9078a47af4353203d825b7763f696452de84c
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-08-30 10:58:43 -0500

    + Agregamos funcion javascript para habilitar y deshabilitar controles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function enable_disable_desc(obj1, obj2, obj3){
+    if(document.getElementById(obj1).checked)
+        document.getElementById(obj3).disabled = false;
+    else
+        if(document.getElementById(obj2).checked)
+            document.getElementById(obj3).disabled = true;
+        else
+            document.getElementById(obj3).disabled = false;
+}
