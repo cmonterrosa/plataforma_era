c4166556f651fbe63965246ef79e3067a610c9ff
Rafael Hernandez Escobar <snakheertax@gmail.com>
2013-09-02 17:05:17 -0500

    Agrega consumos responsables saludables para diagnostico

---
+++ app/controllers/consumos_controller.rb
@@ -0,0 +1,23 @@
+class ConsumosController < ApplicationController
+
+  def new_or_edit
+    @diagnostico = Diagnostico.find(params[:id]) if params[:id]
+    @diagnostico ||= Diagnostico.new
+    @consumo = @diagnostico.consumo || Consumo.new
+  end
+
+  def save
+    @consumo = Consumo.find(params[:id]) if params[:id]
+    @consumo ||= Consumo.new
+    @consumo.update_attributes(params[:consumo])
+    @consumo.diagnostico = Diagnostico.find(params[:diagnostico])
+    if @consumo.save
+      flash[:notice] = "Registro guardado correctamente"
+      redirect_to :controller => "diagnosticos"
+    else
+      flash[:error] = "No se pudo guardar, verifique los datos"
+      render :action => "new_or_edit"
+    end
+  end
+
+end
