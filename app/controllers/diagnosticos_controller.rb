2597f68dd531f162d973deeaa8ea128fb0a44973
Rafael Hernandez Escobar <snakheertax@gmail.com>
2013-09-09 15:21:11 -0500

    Modificaciones para generar reporte de diagnostico

--- app/controllers/diagnosticos_controller.rb
+++ app/controllers/diagnosticos_controller.rb
@@ -1,4 +1,8 @@
+require 'pdfkit'
+
 class DiagnosticosController < ApplicationController
+  layout :set_layout
+
   before_filter :login_required
 
   def index
@@ -13,4 +17,20 @@ class DiagnosticosController < ApplicationController
     redirect_to :action => "index"
   end
 
+  def reporte
+    @diagnostico = Diagnostico.find(params[:id]) if params[:id]
+    @competencia = @diagnostico.competencia if @diagnostico.competencia
+    @entorno = @diagnostico.entorno if @diagnostico.entorno
+
+    kit = PDFKit.new("http://localhost:3000/diagnosticos/reporte/#{@diagnostico.id}")
+    kit.to_file("#{RAILS_ROOT}/tmp/reporte_#{@diagnostico.id}")
+  end
+
+  def set_layout
+    if action_name == 'reporte'
+      'reporte'
+    else
+      'era'
+    end
+  end
 end
