9788e205b80f6fe3a833f4a023271336a79a515f
Rafael Hernandez Escobar <snakheertax@gmail.com>
2013-10-02 07:39:24 -0500

    Modificaciones para validar formulario

--- app/models/competencia.rb
+++ app/models/competencia.rb
@@ -5,7 +5,8 @@ class Competencia < ActiveRecord::Base
 #  before_save :change_to_upcase
 
   def check
-    self.doc_capacitan_salud_desc = "" if self.doc_capacitan_salud == "NO"
+#    self.doc_capacitan_salud_desc = "" if self.doc_capacitan_salud == "NO"
+    self.doc_capacitan_salud_desc.strip!
     self.doc_capacitan_ma_desc = "" if self.doc_capacitan_ma == "NO"
     self.aplica_conocimientos_desc = "" if self.aplica_conocimientos == "NO"
     self.esc_desarrolla_proy_desc = "" if self.esc_desarrolla_proy == "NO"
