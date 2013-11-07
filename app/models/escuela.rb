3f986ea34093ce234597fbff054e0668808b6299
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-11-06 19:55:10 -0600

    + Arregle vista por jquery de datos de escuela y guardado en bitacora

--- app/models/escuela.rb
+++ app/models/escuela.rb
@@ -9,7 +9,7 @@ class Escuela < ActiveRecord::Base
 
   def update_bitacora!(clave_estatus, usuario)
     @estatus = Estatu.find_by_clave(clave_estatus) if (!clave_estatus.nil? && !usuario.nil?)
-    @bitacora = Bitacora.new(:user_id => self.id, :estatu_id => @estatus.id) if @estatus
+    @bitacora = Bitacora.new(:user_id => usuario.id, :estatu_id => @estatus.id) if @estatus
     if @bitacora.save
       #---- actualizacion del registro principal --
       self.update_attributes!(:estatu_id => @estatus.id)
