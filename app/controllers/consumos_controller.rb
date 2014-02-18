9ebc36dd5a88dccb4f902056ae215607bd8fe84d
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-02-18 11:23:00 -0600

    + Adecuacion de validaciones para el eje4 (consumos)

--- app/controllers/consumos_controller.rb
+++ app/controllers/consumos_controller.rb
@@ -11,45 +11,56 @@ class ConsumosController < ApplicationController
     @s_alimentos = multiple_selected(@consumo.alimentos) if @consumo.alimentos
     @s_botanas = multiple_selected(@consumo.botanas) if @consumo.botanas
     @s_reposterias = multiple_selected(@consumo.reposterias) if @consumo.reposterias
+    a=10
   end
 
   def save
     @consumo = Consumo.find(params[:id]) if params[:id]
     @consumo ||= Consumo.new
     @consumo.update_attributes(params[:consumo])
-    @consumo.diagnostico = Diagnostico.find(params[:diagnostico])
-
-    if params[:establecimientos]
-      @establecimientos = []
-      params[:establecimientos].each { |op| @establecimientos << Establecimiento.find_by_clave(op)  }
-      @consumo.establecimientos = Establecimiento.find(@establecimientos)
-    end
-    if params[:bebidas]
-      @bebidas = []
-      params[:bebidas].each { |op| @bebidas << Bebida.find_by_clave(op)  }
-      @consumo.bebidas = Bebida.find(@bebidas)
-    end
-    if params[:alimentos]
-      @alimentos = []
-      params[:alimentos].each { |op| @alimentos << Alimento.find_by_clave(op)  }
-      @consumo.alimentos = Alimento.find(@alimentos)
-    end
-    if params[:botanas]
-      @botanas = []
-      params[:botanas].each { |op| @botanas << Botana.find_by_clave(op)  }
-      @consumo.botanas = Botana.find(@botanas)
-    end
-    if params[:reposterias]
-      @reposterias = []
-      params[:reposterias].each { |op| @reposterias << Reposteria.find_by_clave(op)  }
-      @consumo.reposterias = Reposteria.find(@reposterias)
-    end
-
-    if @consumo.save
-      flash[:notice] = "Registro guardado correctamente"
-      redirect_to :controller => "diagnosticos"
+    @diagnostico = @consumo.diagnostico = Diagnostico.find(params[:diagnostico])
+    validador = verifica_evidencias(@diagnostico,4)
+    if validador["valido"]
+          if params[:establecimientos]
+            @establecimientos = []
+            params[:establecimientos].each { |op| @establecimientos << Establecimiento.find_by_clave(op)  }
+            @consumo.establecimientos = Establecimiento.find(@establecimientos)
+          end
+          if params[:bebidas]
+            @bebidas = []
+            params[:bebidas].each { |op| @bebidas << Bebida.find_by_clave(op)  }
+            @consumo.bebidas = Bebida.find(@bebidas)
+          end
+        if params[:alimentos]
+          @alimentos = []
+          params[:alimentos].each { |op| @alimentos << Alimento.find_by_clave(op)  }
+          @consumo.alimentos = Alimento.find(@alimentos)
+        end
+        if params[:botanas]
+          @botanas = []
+          params[:botanas].each { |op| @botanas << Botana.find_by_clave(op)  }
+          @consumo.botanas = Botana.find(@botanas)
+        end
+        if params[:reposterias]
+          @reposterias = []
+          params[:reposterias].each { |op| @reposterias << Reposteria.find_by_clave(op)  }
+          @consumo.reposterias = Reposteria.find(@reposterias)
+        end
+        if @consumo.save
+          flash[:notice] = "Registro guardado correctamente"
+          redirect_to :controller => "diagnosticos"
+        else
+          flash[:error] = "No se pudo guardar, verifique los datos"
+          render :action => "new_or_edit"
+        end
     else
-      flash[:error] = "No se pudo guardar, verifique los datos"
+      @s_establecimientos = multiple_selected(@consumo.establecimientos) if @consumo.establecimientos
+      @s_bebidas = multiple_selected(@consumo.bebidas) if @consumo.bebidas
+      @s_alimentos = multiple_selected(@consumo.alimentos) if @consumo.alimentos
+      @s_botanas = multiple_selected(@consumo.botanas) if @consumo.botanas
+      @s_reposterias = multiple_selected(@consumo.reposterias) if @consumo.reposterias
+      errores = validador["sin_validar"].join(" y ")
+      flash[:evidencias] = "Cargue archivos para la(s) pregunta(s): #{errores}"
       render :action => "new_or_edit"
     end
   end
