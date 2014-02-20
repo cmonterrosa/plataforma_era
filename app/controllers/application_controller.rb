29df78de3e3cc0dc284ae420190227b161061831
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-02-20 12:44:36 -0600

    + Agregue layout con banner donde aparece gobernador

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  layout 'era', :except => :sessions
+  layout 'era2014'#, :except => :sessions
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  layout 'era'
+  
   
   def index
   end
