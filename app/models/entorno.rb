708da3f7ef55e8b5083e3e7044cc7ad553d72d11
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2014-02-21 03:18:27 -0600

    + Simplifique modelo de modelo de entorno

--- app/models/entorno.rb
+++ app/models/entorno.rb
@@ -2,10 +2,6 @@ class Entorno < ActiveRecord::Base
   belongs_to :diagnostico
   has_and_belongs_to_many :acciones, :join_table => 'entornos_acciones'
 
-#  before_save :check_evidencias
-
-#  validates_presence_of :jardines_verticales, :if => :tiene_evidencia?
-
   validates_presence_of :evidencia_pregunta_3, :if => "self.jardines_verticales == 'SI'"
   validates_presence_of :evidencia_pregunta_4, :if => "self.jardines_hidroponicos == 'SI'"
 
