1deb43e9cae4551b25ec55454ec25374e76be4a1
Rafael Hernandez Escobar <snakheertax@gmail.com>
2014-03-04 22:56:42 -0600

    Agrega botones para editar proyecto y ejes

--- app/controllers/ejes_controller.rb
+++ app/controllers/ejes_controller.rb
@@ -3,8 +3,8 @@ class EjesController < ApplicationController
   end
 
   def save_eje
-     @eje = Eje.find(params[:id]) if params[:id]
-     @eje ||= Eje.new
+    @eje = Eje.find(params[:id]) if params[:id]
+    @eje ||= Eje.new
 
     @lineas_accion_id = LineasAccion.find_by_clave(params[:eje][:lineas_accion_id]).id if params[:eje][:lineas_accion_id]
     params[:eje][:lineas_accion_id] = @lineas_accion_id if  @lineas_accion_id
@@ -17,11 +17,11 @@ class EjesController < ApplicationController
 
     params[:eje][:proyecto_id] = params[:proyecto] if params[:proyecto]
     
-     if @eje.update_attributes(params[:eje])
+    if @eje.update_attributes(params[:eje])
        redirect_to :controller => "proyectos", :action => "index"
-     else
+    else
        flash[:error] = "No se pudo crear el eje a desarrollar"
-     end
+    end
   end
 
 end
