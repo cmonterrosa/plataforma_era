9291ea839ae46bbcc06a584b87f568038d421d6c
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-03-18 12:40:55 -0600

    + Optimizacion de metodo update bitacora

--- app/models/escuela.rb
+++ app/models/escuela.rb
@@ -14,10 +14,15 @@ class Escuela < ActiveRecord::Base
   def update_bitacora!(clave_estatus, usuario)
     @estatus = Estatu.find_by_clave(clave_estatus) if (!clave_estatus.nil? && !usuario.nil?)
     @bitacora = Bitacora.new(:user_id => usuario.id, :estatu_id => @estatus.id) if @estatus
-    if @bitacora.save
-      #---- actualizacion del registro principal --
-      self.update_attributes!(:estatu_id => @estatus.id)
-      return true
+    @existe_registro = Escuela.find(:first, :conditions => ["id = ? AND estatu_id = ?", self.id, @estatus.id])
+    unless @existe_registro
+      if @bitacora.save
+        #---- actualizacion del registro principal --
+        self.update_attributes!(:estatu_id => @estatus.id)
+        return true
+      end
+    else
+      return false
     end
   end
 
