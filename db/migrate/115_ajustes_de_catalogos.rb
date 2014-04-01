b123e66a2445952030d9248883c7994a2d881d3b
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-04-01 11:47:00 -0600

    + Agregue nuevos campos para control de archivos adjuntos

--- db/migrate/115_ajustes_de_catalogos.rb
+++ db/migrate/115_ajustes_de_catalogos.rb
@@ -1,9 +1,17 @@
 class AjustesDeCatalogos < ActiveRecord::Migration
   def self.up
     Programa.create(:clave => "NIN", :descripcion => "NINGUNO")
+    add_column :adjuntos, :validado, :boolean
+    add_column :adjuntos, :user_validado, :integer
+    add_column :diagnosticos, :validacion_evidencias, :boolean
+    add_column :diagnosticos, :observaciones_evidencias, :string, :limit => 180
   end
 
   def self.down
-    Programa.find_by_clave("NIN").destroy
+    Programa.find_by_clave("NIN").destroy if Programa.find_by_clave("NIN")
+    remove_columns :adjuntos, :validado
+    remove_columns :adjuntos, :user_validado
+    remove_columns :diagnosticos, :validacion_evidencias
+    remove_columns :diagnosticos, :observaciones_evidencias
   end
 end
