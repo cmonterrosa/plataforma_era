65532753102e1fd5f47b38205a333e4f901b8d22
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-04-10 11:23:03 -0500

    + Modificacion de agrupamiento de niveles educativos

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,12 +1,14 @@
 class PublicController < ApplicationController
 
   def report_by_niveles_libre
-    @niveles = Escuela.find(:all, :group => "nivel_descripcion", :conditions => ["estatu_id IS NOT NULL"])
+    #@niveles = Escuela.find(:all, :group => "nivel_descripcion", :conditions => ["estatu_id IS NOT NULL"])
+    @niveles = Nivel.find(:all, :conditions => ["descripcion <> ?", "OTRO"])
   end
 
   def list_esc_nivel
-    @nivel = params[:nivel_descripcion]
-    @escuelas = User.find(:all, :select => "users.login, users.id as user_id, e.*", :joins => "users, escuelas e", :conditions => ["users.login=e.clave AND (users.blocked is NULL OR  users.blocked !=1) and e.estatu_id > 0 and e.nivel_descripcion = ?", params[:nivel_descripcion]], :order => "e.modalidad").paginate(:page => params[:page], :per_page => 15)
+    @nivel_descripcion = params[:nivel_descripcion]
+    @nivel = Nivel.find_by_descripcion(@nivel_descripcion)
+    @escuelas = User.find(:all, :select => "users.login, users.id as user_id, e.*", :joins => "users, escuelas e", :conditions => ["users.login=e.clave AND (users.blocked is NULL OR  users.blocked !=1) and e.estatu_id > 0 and e.nivel_id = ?", @nivel.id], :order => "e.modalidad").paginate(:page => params[:page], :per_page => 15)
    end
 
 end
--- db/migrate/116_fusionar_tabla_niveles_educativos.rb
+++ db/migrate/116_fusionar_tabla_niveles_educativos.rb
@@ -10,38 +10,63 @@ class FusionarTablaNivelesEducativos < ActiveRecord::Migration
    end
 
    puts("=> Truncamos tabla")
-   ActiveRecord::Base.connection.execute("TRUNCATE TABLE NIVELS")
+   ActiveRecord::Base.connection.execute("truncate table nivels")
 
 
    ### Creamos catalogo de nuevo
-   ms = Nivel.create(:clave => 1, :descripcion => "MEDIA SUPERIOR")
-   sec = Nivel.create(:clave => 2, :descripcion => "SECUNDARIA")
-   pri = Nivel.create(:clave => 3, :descripcion => "PRIMARIA")
-   pre = Nivel.create(:clave => 4, :descripcion => "PREESCOLAR")
+   pre = Nivel.create(:clave => 1, :descripcion => "PREESCOLAR")
+   pri = Nivel.create(:clave => 2, :descripcion => "PRIMARIA")
+   sec = Nivel.create(:clave => 3, :descripcion => "SECUNDARIA")
+   ms = Nivel.create(:clave => 4, :descripcion => "MEDIA SUPERIOR")
    com = Nivel.create(:clave => 5, :descripcion => "COMUNITARIA")
    esp = Nivel.create(:clave => 6, :descripcion => "ESPECIAL")
    otr = Nivel.create(:clave => 7, :descripcion => "OTRO")
 
 
-   puts("=> Limpiamos campo nivel_id de tabla escuelas") 
-   escuelas = Escuela.find(:all)
-   escuelas.each do |e| e.update_attributes!(:nivel_id => nil) end
+   #puts("=> Limpiamos campo nivel_id de tabla escuelas")
+   #   escuelas = Escuela.find(:all)
+   #   escuelas.each do |e| e.update_attributes!(:nivel_id => nil) end
 
-
-   puts("=> Vinculamos escuelas")
    puts("=> Actualizando Media Superior")
-   all_ms = Escuela.find(:all, :conditions => ["nivel_descripcion in (?)", %w "BACHILLERATO, PROFESIONALMEDIO, TECNICO MEDIO CONALEP ESTATAL"])
-   all_ms.each do |e| e.update_attributes!(:nivel_id => ms) end
+   bachillerato = ["BACHILLERATO", "PROFESIONALMEDIO", "TECNICO MEDIO CONALEP ESTATAL"]
+   all_ms = Escuela.find(:all, :conditions => ["nivel_descripcion in (?)", bachillerato])
+   all_ms.each do |e| e.update_attributes!(:nivel_id => ms.id) end
    puts("=> Media Superior Actualizadas: #{all_ms.size}")
 
-   ##### Borramos todos los niveles educativos que no van a mostrarse ######
+   puts("=> Actualizando Secundaria")
+   all_sec = Escuela.find(:all, :conditions => ["nivel_descripcion like ?", "SECUND%"])
+   all_sec.each do |e| e.update_attributes!(:nivel_id => sec.id) end
+   puts("=> Secundaria Actualizadas: #{all_sec.size}")
 
-  
+   puts("=> Actualizando Primaria")
+   all_pri = Escuela.find(:all, :conditions => ["nivel_descripcion like ?", "PRIMARI%"])
+   all_pri.each do |e| e.update_attributes!(:nivel_id => pri.id) end
+   puts("=> Primaria Actualizadas: #{all_pri.size}")
+
+   puts("=> Actualizando Preescolar")
+   all_pre = Escuela.find(:all, :conditions => ["nivel_descripcion like ?", "PREESCOLAR%"])
+   all_pre.each do |e| e.update_attributes!(:nivel_id => pre.id) end
+   puts("=> Primaria Actualizadas: #{all_pre.size}")
 
 
+   puts("=> Actualizando Comunitarias")
+   comunitarias = %w{EXTRAESCOLAR COMUNITARIA INICIAL}
+   all_com = Escuela.find(:all, :conditions => ["nivel_descripcion in (?)", comunitarias])
+   all_com.each do |e| e.update_attributes!(:nivel_id => com.id) end
+   puts("=> Media Superior Actualizadas: #{all_com.size}")
 
 
-  end
+   puts("=> Actualizando Especial")
+   all_esp = Escuela.find(:all, :conditions => ["nivel_descripcion like ?", "ESPECIA%"])
+   all_esp.each do |e| e.update_attributes!(:nivel_id => esp.id) end
+   puts("=> Primaria Actualizadas: #{all_esp.size}")
+
+   puts("=> Actualizando las demás")
+   sobrantes = Escuela.find(:all, :conditions => ["nivel_descripcion IS NULL"])
+   sobrantes.each do |s| s.update_attributes!(:nivel_id => otr.id) end
+
+   puts(" => Finalizando")
+ end
 
   def self.down
   end
