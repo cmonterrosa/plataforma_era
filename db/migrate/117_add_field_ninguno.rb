df05548b79465b705a10f0fabcc0dc6e98bb64f5
Rafael Hernandez Escobar <snakheertax@gmail.com>
2014-04-10 20:03:28 -0500

    Modificaciones correo 8 de abril

---
+++ db/migrate/117_add_field_ninguno.rb
@@ -0,0 +1,11 @@
+class AddFieldNinguno < ActiveRecord::Migration
+  def self.up
+#    Programa.create(:descripcion => "NINGUNO", :clave => "NIN")
+    Accione.create(:descripcion =>"NINGUNA", :clave => "NING")
+  end
+
+  def self.down
+    Programa.find_by_descripcion("NINGUNO").destroy
+    Accione.find_by_descripcion("NINGUNA").destroy
+  end
+end
