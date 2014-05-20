7b4b2dac766dc09df186d75fa26864b11bfbf148
Rafael Hernandez Escobar <snakheertax@gmail.com>
2014-05-20 13:15:19 -0500

    Correccion de la validacion en el modelo de huella

--- app/models/huella.rb
+++ app/models/huella.rb
@@ -5,21 +5,21 @@ class Huella < ActiveRecord::Base
   has_and_belongs_to_many :inorganicos, :join_table => 'huellas_inorganicos'
   has_and_belongs_to_many :elimina_residuos, :join_table => 'huellas_elimina_residuos'
 
-  validates_presence_of :evidencia_pregunta_1, :if => "self.capacitacion_ahorro_energia > 0"
-  validates_presence_of :evidencia_pregunta_2, :if => "self.consumo_anterior > 0 and self.consumo_actual > 0"
+  validates_presence_of :evidencia_pregunta_1, :if => "self.capacitacion_ahorro_energia.to_i > 0"
+  validates_presence_of :evidencia_pregunta_2, :if => "self.consumo_anterior.to_i > 0 and self.consumo_actual.to_i > 0"
   
 
   def evidencia_pregunta_1
     current_eje = 3
     contador = Adjunto.count(:id, :conditions => ["eje_id = ? AND diagnostico_id = ? AND numero_pregunta = ?", current_eje, self.diagnostico_id, 1])
     #self.errors.add(:pregunta_3, "=> Requiere evidencia") if contador < 1
-    (contador > 0)?  true : false
+    (contador.to_i > 0)?  true : false
   end
 
   def evidencia_pregunta_2
     current_eje = 3
     contador = Adjunto.count(:id, :conditions => ["eje_id = ? AND diagnostico_id = ? AND numero_pregunta = ?", current_eje, self.diagnostico_id, 2])
     #self.errors.add(:pregunta_3, "=> Requiere evidencia") if contador < 1
-    (contador > 0)?  true : false
+    (contador.to_i > 0)?  true : false
   end
 end
