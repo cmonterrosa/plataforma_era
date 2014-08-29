6049ba285ce16f51fca38384dd4586ea71f7900c
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2014-08-28 20:51:22 -0500

    + Agregue indice de busqeuda de evaluaciones

---
+++ db/migrate/129_create_evaluacions.rb
@@ -0,0 +1,22 @@
+class CreateEvaluacions < ActiveRecord::Migration
+  def self.up
+    create_table :evaluacions do |t|
+      t.integer :diagnostico_id
+      t.decimal :puntaje_eje1, :precision=> 15, :scale=> 2
+      t.decimal :puntaje_eje2, :precision=> 15, :scale=> 2
+      t.decimal :puntaje_eje3, :precision=> 15, :scale=> 2
+      t.decimal :puntaje_eje4, :precision=> 15, :scale=> 2
+      t.string :observaciones
+      t.boolean :activa
+      t.integer :user_id #Evaluador
+      t.timestamps
+    end
+   add_index :evaluacions, [:diagnostico_id, :activa], :name => "diagnostico_activa"
+   add_index :evaluacions, [:diagnostico_id, :user_id, :activa], :name => "diagnostico_user_activa"
+  end
+
+  def self.down
+    drop_table :evaluacions
+  end
+end
+
