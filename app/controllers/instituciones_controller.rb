1190922bf55d5cf0af155042b990986a74dca95e
Carlos Monterrosa <cmonterrosa@poderjudicialchiapas.gob.mx>
2014-09-11 13:39:17 -0500

    + validaciones en instituciones

--- app/controllers/instituciones_controller.rb
+++ app/controllers/instituciones_controller.rb
@@ -8,12 +8,12 @@ def index
       @proyectos_evaluados = Evaluacion.find(:all, :select => "proyecto_id", :conditions => ["diagnostico_id IS NULL AND user_id = ?", current_user.id], :group => "proyecto_id")
       @escuelas_diagnosticos_evaluados = Escuela.find(:all, :select => "escuelas.*", :joins => "escuelas, diagnosticos d", :conditions => ["escuelas.id = d.escuela_id AND d.id in (?)", @diagnosticos_evaluados.map { |i|i.diagnostico_id  }])
       @escuelas_diagnosticos_proyectos = Escuela.find(:all, :select => "escuelas.*", :joins => "escuelas, diagnosticos d, proyectos p", :conditions => ["escuelas.id = d.escuela_id AND d.id = p.diagnostico_id AND p.id in (?)", @proyectos_evaluados.map { |i|i.proyecto_id  }])
-      @escuelas_diagnosticos_evaluados.each{|e| repetidas << @escuelas_diagnosticos_proyectos === e }
+      @escuelas_diagnosticos_evaluados.each{|e| repetidas << @escuelas_diagnosticos_proyectos === e } if (!@escuelas_diagnosticos_proyectos.empty? && ! @escuelas_diagnosticos_evaluados.empty?)
       
    if @condition =="evaluadas"
-      @escuelas = Escuela.find(:all, :conditions => ["evaluador_id = ? AND id in (?)", current_user.id, repetidas[0].map{|i|i.id}])
+      @escuelas = Escuela.find(:all, :conditions => ["evaluador_id = ? AND id in (?)", current_user.id, repetidas[0].map{|i|i.id}]) if !repetidas.empy?
    else
-     @escuelas = Escuela.find(:all, :conditions => ["evaluador_id = ? AND id not in (?)", current_user.id, repetidas[0].map{|i|i.id}])
+     @escuelas = Escuela.find(:all, :conditions => ["evaluador_id = ? AND id not in (?)", current_user.id, repetidas[0].map{|i|i.id}]) if !repetidas.empty?
    end
    @escuelas = @escuelas.paginate(:page => params[:page], :per_page => 25) if @escuelas
 end
