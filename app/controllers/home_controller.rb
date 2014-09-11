2d90cea4f342ee659e132416c91c9d15468606f2
Carlos Monterrosa <cmonterrosa@poderjudicialchiapas.gob.mx>
2014-09-11 13:07:42 -0500

    + Cambio a string

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,11 +42,11 @@ class HomeController < ApplicationController
 protected
 def es_revisor?
       if @usuario=current_user
-        if @usuario.has_role?(:admin) || @usuario.has_role?(:enlaceevaluador)
+        if @usuario.has_role?("admin") || @usuario.has_role?("enlaceevaluador")
             redirect_to :controller => "admin"
         else
-            if current_user.has_role?(:revisor)
-              redirect_to :controller => "instituciones"
+            if current_user.has_role?("revisor")
+                redirect_to :controller => "instituciones"
             end
         end
       end
