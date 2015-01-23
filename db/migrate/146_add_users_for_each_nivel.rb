b4bd867204c4a25e808ca8a20f95824a472d37e5
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-01-23 13:08:33 -0600

    + Agregue division de usuarios de niveles por subsistemas

--- db/migrate/146_add_users_for_each_nivel.rb
+++ db/migrate/146_add_users_for_each_nivel.rb
@@ -2,6 +2,7 @@ class AddUsersForEachNivel < ActiveRecord::Migration
   def self.up
     Role.create(:name => "nivel", :descripcion => "Nivel Educativo") unless Role.find_by_name("nivel")
     add_column :users, :nivel_id, :integer
+    add_column :users, :sostenimiento, :string, :limit => 10
 
     ### Reorganizo algunos centros de trabajo que no tienen campo de nivel #####
     esc_primarias = Escuela.find(:all, :conditions => ["nombre like ? AND nivel_id IS NULL", 'PRIMARIA%'])
@@ -17,27 +18,30 @@ class AddUsersForEachNivel < ActiveRecord::Migration
     esc_otros = Escuela.find(:all, :conditions => ["nivel_id IS NULL"])
     esc_otros.each do |e| e.update_attributes!(:nivel_id => Nivel.find_by_descripcion("OTRO").id) end
 
-    #### Crear usuarios por cada nivel encontrado #####
-    Nivel.find(:all).each do |nivel|
-      login = nivel.descripcion.gsub(/\s/, '')
-      puts login
-      password = "#{login}#{nivel.id.to_s}" if login
-      puts password
-      usuario = User.new(:login => login,
-                   :nombre => "Usuario de #{nivel.descripcion}",
-                   :password => password.downcase,
-                   :password_confirmation => password.downcase,
-                   :email => "#{login.downcase}@educacionchiapas.gob.mx",
-                   :activated_at => Time.now,
-                   :nivel_id => nivel.id
+    
+    #### Crear usuarios por cada nivel encontrado y subsistema #####
+    @sostenimientos = ["FEDERAL", "ESTATAL"]
+    @sostenimientos.each do |subsistema|
+            Nivel.find(:all).each do |nivel|
+                login =  subsistema.split("").first +  nivel.descripcion.gsub(/\s/, '')
+                password = "#{login}#{nivel.id.to_s}" if login
+                usuario = User.new(:login => login,
+                      :nombre => "Usuario de #{nivel.descripcion} #{subsistema}",
+                      :password => password.downcase,
+                      :password_confirmation => password.downcase,
+                      :email => "#{login.downcase}@educacionchiapas.gob.mx",
+                      :activated_at => Time.now,
+                      :nivel_id => nivel.id,
+                      :sostenimiento => subsistema
                    ) if login && password
-      usuario.roles << Role.find_by_name("nivel") if usuario
-      puts("=> Usuario: #{usuario.login} - Password:#{usuario.password}") if (usuario && usuario.save) && usuario.activate!
+                usuario.roles << Role.find_by_name("nivel") if usuario
+                puts("=> Usuario: #{usuario.login} - Password:#{usuario.password}") if (usuario && usuario.save) && usuario.activate!
+           end
+      end
 
 
 
-    end
-
+ 
 
   end
 
@@ -48,5 +52,6 @@ class AddUsersForEachNivel < ActiveRecord::Migration
   def self.down
     Role.find_by_name("nivel").destroy if Role.find_by_name("nivel")
     remove_column :users, :nivel_id
+    remove_column :users, :sistenimiento
   end
 end
