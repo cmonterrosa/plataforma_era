362805239164f2321da989e3662762e5f3153868
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2015-02-18 20:32:42 -0600

    + Ajuste para contabilizar md5 creados de archivos

--- db/migrate/159_add_cksum_to_adjuntos.rb
+++ db/migrate/159_add_cksum_to_adjuntos.rb
@@ -1,14 +1,15 @@
 class AddCksumToAdjuntos < ActiveRecord::Migration
   def self.up
     add_column :adjuntos, :md5, :string, :limit => 32
-    add_index :adjuntos, :md5, :name => 'cksum'
+    add_index :adjuntos, :md5, :name => 'cksum', :unique => true
 
     puts "=> Creamos md5 para los archivos existentes"
     contador_archivos=0
     Adjunto.find(:all).each do |adjunto|
-      md5 = (adjunto.make_md5) ? adjunto.save : false
+      md5 = (adjunto.make_md5) ? true : false
       (md5)? contador_archivos+=1 : nil
     end
+    puts "=> Total de archivos procesados: #{contador_archivos}"
   end
 
   def self.down
