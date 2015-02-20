4bc190ef646aebc1e869ab040c9b1bdd9e3b142a
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-02-20 15:51:32 -0600

    + Migracion para ajustes de eje 4 (consumos)

---
+++ db/migrate/160_ajuste2015_eje4.rb
@@ -0,0 +1,11 @@
+class Ajuste2015Eje4 < ActiveRecord::Migration
+  def self.up
+    puts("=> Ajuste del Eje 4 del diagnostico, ciclo 2014-2015")
+    # Pregunta 1
+    Establecimiento.create(:clave => "CCCT", :nivel => "BASICA", :descripcion => "LA ESCUELA CUENTA CON CAFETERIA O TIENDA ESCOLAR" )
+  end
+
+  def self.down
+    Establecimiento.find_by_clave("CCCT").destroy if Establecimiento.find_by_clave("CCCT")
+  end
+end
