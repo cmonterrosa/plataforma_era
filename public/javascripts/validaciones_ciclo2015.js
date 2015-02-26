8bdbb217a58c6749ea39897e60b5b5e8844c8915
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2015-02-26 04:05:09 -0600

    + Nuevas funciones javascript

--- public/javascripts/validaciones_ciclo2015.js
+++ public/javascripts/validaciones_ciclo2015.js
@@ -35,6 +35,58 @@ function enableDisableDivWithFielset (fieldset_main, preguntas, valores_permitid
 }
 
 
+/* Funciones usadas en EJE 4 */
+
+function showPreguntasEstablecimientos(fieldset_main, p2, p3, p4, p5, p6, p7,p8,p9,p10,p11){
+      fieldset = document.getElementById(fieldset_main);
+      pregunta2 = document.getElementById(p2);
+      pregunta3 = document.getElementById(p3);
+      pregunta4 = document.getElementById(p4);
+      pregunta5 = document.getElementById(p5);
+      pregunta6 = document.getElementById(p6);
+      pregunta7 = document.getElementById(p7);
+      pregunta8 = document.getElementById(p8);
+      pregunta9 = document.getElementById(p9);
+      pregunta10 = document.getElementById(p10);
+      pregunta11 = document.getElementById(p11);
+      for(var i=0;i<fieldset.elements.length;i++) {
+        if(fieldset.elements[i].type == 'checkbox' && fieldset.elements[i].checked && (fieldset.elements[i].value == "CLAE" || fieldset.elements[i].value == "CAEC" ) ){
+              pregunta2.style.display = "none";
+              pregunta3.style.display = "none";
+              pregunta4.style.display = "none";
+              pregunta5.style.display = "none";
+              pregunta6.style.display = "none";
+              pregunta7.style.display = "none";
+              pregunta8.style.display = "none";
+              pregunta9.style.display = "none";
+              pregunta10.style.display = "none";
+              pregunta11.style.display = "none";
+              break;
+            }
+            else{
+                pregunta2.style.display = "inline";
+                pregunta3.style.display = "inline";
+                pregunta4.style.display = "inline";
+                pregunta5.style.display = "inline";
+                pregunta6.style.display = "inline";
+                pregunta7.style.display = "inline";
+                pregunta8.style.display = "inline";
+                pregunta9.style.display = "inline";
+                pregunta10.style.display = "inline";
+                pregunta11.style.display = "inline";
+                }
+      }
+}
+
+
+
+
+
+
+
+
+/* Funciones usadas en EJE 5 */
+
 function sumatoria_dos_elementos(elemento1, elemento2, resultado){
  total = parseInt(document.getElementById(elemento1).value)+parseInt(document.getElementById(elemento2).value);
  document.getElementById(resultado).innerHTML = total;
@@ -50,7 +102,6 @@ function enable_disable_selects (checkbox_element, select_element,divEv) {
             document.getElementById(divEv).style.display = "none";
         }
 }
-
 
 function showImageFieldset(fieldset_main, divEv){
       fieldset = document.getElementById(fieldset_main);
