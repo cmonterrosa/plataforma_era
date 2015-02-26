d1a5e5994894b560a78bfb72f12b9c5757b6144d
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-02-26 15:56:56 -0600

    + Ajustes de validaciones

--- public/javascripts/validaciones_ciclo2015.js
+++ public/javascripts/validaciones_ciclo2015.js
@@ -37,47 +37,85 @@ function enableDisableDivWithFielset (fieldset_main, preguntas, valores_permitid
 
 /* Funciones usadas en EJE 4 */
 
-function showPreguntasEstablecimientos(fieldset_main, p2, p3, p4, p5, p6, p7,p8,p9,p10,p11){
+//function showPreguntasEstablecimientos(fieldset_main, p2, p3, p4, p5, p6, p7,p8,p9,p10,p11){
+//      fieldset = document.getElementById(fieldset_main);
+//      pregunta2 = document.getElementById(p2);
+//      pregunta3 = document.getElementById(p3);
+//      pregunta4 = document.getElementById(p4);
+//      pregunta5 = document.getElementById(p5);
+//      pregunta6 = document.getElementById(p6);
+//      pregunta7 = document.getElementById(p7);
+//      pregunta8 = document.getElementById(p8);
+//      pregunta9 = document.getElementById(p9);
+//      pregunta10 = document.getElementById(p10);
+//      for(var i=0;i<fieldset.elements.length;i++) {
+//        if(fieldset.elements[i].type == 'checkbox' && fieldset.elements[i].checked && (fieldset.elements[i].value == "CLAE" || fieldset.elements[i].value == "CAEC" ) ){
+//              pregunta2.style.display = "none";
+//              pregunta3.style.display = "none";
+//              pregunta4.style.display = "none";
+//              pregunta5.style.display = "none";
+//              pregunta6.style.display = "none";
+//              pregunta7.style.display = "none";
+//              pregunta8.style.display = "none";
+//              pregunta9.style.display = "none";
+//              pregunta10.style.display = "none";
+//              break;
+//            }
+//            else{
+//                pregunta2.style.display = "inline";
+//                pregunta3.style.display = "inline";
+//                pregunta4.style.display = "inline";
+//                pregunta5.style.display = "inline";
+//                pregunta6.style.display = "inline";
+//                pregunta7.style.display = "inline";
+//                pregunta8.style.display = "inline";
+//                pregunta9.style.display = "inline";
+//                pregunta10.style.display = "inline";
+//               }
+//      }
+//}
+
+
+
+function showPreguntasEstablecimientos(fieldset_main){
       fieldset = document.getElementById(fieldset_main);
-      pregunta2 = document.getElementById(p2);
-      pregunta3 = document.getElementById(p3);
-      pregunta4 = document.getElementById(p4);
-      pregunta5 = document.getElementById(p5);
-      pregunta6 = document.getElementById(p6);
-      pregunta7 = document.getElementById(p7);
-      pregunta8 = document.getElementById(p8);
-      pregunta9 = document.getElementById(p9);
-      pregunta10 = document.getElementById(p10);
-      pregunta11 = document.getElementById(p11);
+      pregunta2 = document.getElementById("pregunta2");
+      pregunta3 = document.getElementById("pregunta3");
+      pregunta4 = document.getElementById("pregunta4");
+      preparacion_utensilios = document.getElementById("preparacion_utensilios");
+      higiene = document.getElementById("higiene");
+      productos = document.getElementById("productos");
+      bebidas_alimentos = document.getElementById("bebidas_alimentos");
+      botanas_reposterias = document.getElementById("botanas_reposterias");
+      materiales = document.getElementById("materiales");
       for(var i=0;i<fieldset.elements.length;i++) {
         if(fieldset.elements[i].type == 'checkbox' && fieldset.elements[i].checked && (fieldset.elements[i].value == "CLAE" || fieldset.elements[i].value == "CAEC" ) ){
               pregunta2.style.display = "none";
               pregunta3.style.display = "none";
               pregunta4.style.display = "none";
-              pregunta5.style.display = "none";
-              pregunta6.style.display = "none";
-              pregunta7.style.display = "none";
-              pregunta8.style.display = "none";
-              pregunta9.style.display = "none";
-              pregunta10.style.display = "none";
-              pregunta11.style.display = "none";
+              preparacion_utensilios.style.display = "none";
+              higiene.style.display = "none";
+              productos.style.display = "none";
+              bebidas_alimentos.style.display = "none";
+              botanas_reposterias.style.display = "none";
+              materiales.style.display = "none";
+              alert("ok");
               break;
             }
             else{
-                pregunta2.style.display = "inline";
-                pregunta3.style.display = "inline";
-                pregunta4.style.display = "inline";
-                pregunta5.style.display = "inline";
-                pregunta6.style.display = "inline";
-                pregunta7.style.display = "inline";
-                pregunta8.style.display = "inline";
-                pregunta9.style.display = "inline";
-                pregunta10.style.display = "inline";
-                pregunta11.style.display = "inline";
-                }
+               pregunta2.style.display = "inline";
+              pregunta3.style.display =  "inline";
+              pregunta4.style.display =  "inline";
+              preparacion_utensilios.style.display =  "inline";
+              higiene.style.display =  "inline";
+              productos.style.display =  "inline";
+              bebidas_alimentos.style.display =  "inline";
+              botanas_reposterias.style.display =  "inline";
+              materiales.style.display =  "inline";
+                 alert("habilita preguntas");
+               }
       }
 }
-
 
 
 
