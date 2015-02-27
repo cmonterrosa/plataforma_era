908704ccd95b2ded57fa62774d8eb715344e29c4
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-02-27 13:04:02 -0600

    + Modificaciones javascript

--- public/javascripts/validaciones_ciclo2015.js
+++ public/javascripts/validaciones_ciclo2015.js
@@ -1,81 +1,6 @@
-/****** Validaciones Diagnostico 2015 ********/
+/****** VALIDACIONES CICLO ESCOLAR 2014-2015 ********/
 
-
-/* Habilita o desahabilita un div si check tiene valores permitidos */
-function enableDisableDivWithFielset (fieldset_main, preguntas, valores_permitidos) {
-    var fieldset = document.getElementById(fieldset_main);
-    var oculto = "none";
-    var visible = "inline";
-
-    for(var i=0;i<fieldset.elements.length;i++) {
-//        alert(fieldset.elements[i].value);
-        /*Iteramos sobre valores permitidos*/
-        for(valor=0;valor<valores_permitidos.length;valor++){
-           if(fieldset.elements[i].value == valores_permitidos[valor]){
-                /* Cambiamos estilos de las preguntas a ocultos*/
-                for(pregunta=0;pregunta<preguntas.length;pregunta++){
-                    alert("Pregunta ocultada:  " + preguntas[pregunta] + ", Por valor: " + fieldset.elements[i].value );
-                    document.getElementById(preguntas[pregunta]).style.display = oculto;
-                    break;
-                }
-            }
-            else{
-                    /* Cambiamos estilos de las preguntas a visibles*/
-                    for(pregunta=0;pregunta<preguntas.length;pregunta++){
-                         //alert("Pregunta visivle: " + preguntas[pregunta] + ", Por valor:" + fieldset.elements[i].value );
-                        
-                        }
-                  }
-
-        }
-    }
-    /*document.getElementById(box).style.display = vis; */
-    return oculto;
-
-}
-
-
-/* Funciones usadas en EJE 4 */
-
-//function showPreguntasEstablecimientos(fieldset_main, p2, p3, p4, p5, p6, p7,p8,p9,p10,p11){
-//      fieldset = document.getElementById(fieldset_main);
-//      pregunta2 = document.getElementById(p2);
-//      pregunta3 = document.getElementById(p3);
-//      pregunta4 = document.getElementById(p4);
-//      pregunta5 = document.getElementById(p5);
-//      pregunta6 = document.getElementById(p6);
-//      pregunta7 = document.getElementById(p7);
-//      pregunta8 = document.getElementById(p8);
-//      pregunta9 = document.getElementById(p9);
-//      pregunta10 = document.getElementById(p10);
-//      for(var i=0;i<fieldset.elements.length;i++) {
-//        if(fieldset.elements[i].type == 'checkbox' && fieldset.elements[i].checked && (fieldset.elements[i].value == "CLAE" || fieldset.elements[i].value == "CAEC" ) ){
-//              pregunta2.style.display = "none";
-//              pregunta3.style.display = "none";
-//              pregunta4.style.display = "none";
-//              pregunta5.style.display = "none";
-//              pregunta6.style.display = "none";
-//              pregunta7.style.display = "none";
-//              pregunta8.style.display = "none";
-//              pregunta9.style.display = "none";
-//              pregunta10.style.display = "none";
-//              break;
-//            }
-//            else{
-//                pregunta2.style.display = "inline";
-//                pregunta3.style.display = "inline";
-//                pregunta4.style.display = "inline";
-//                pregunta5.style.display = "inline";
-//                pregunta6.style.display = "inline";
-//                pregunta7.style.display = "inline";
-//                pregunta8.style.display = "inline";
-//                pregunta9.style.display = "inline";
-//                pregunta10.style.display = "inline";
-//               }
-//      }
-//}
-
-
+/* Funciones usadas en Eje 4 */
 
 function showPreguntasEstablecimientos(fieldset_main){
       fieldset = document.getElementById(fieldset_main);
@@ -99,8 +24,7 @@ function showPreguntasEstablecimientos(fieldset_main){
                 bebidas_alimentos.style.display = "none";
                 botanas_reposterias.style.display = "none";
                 materiales.style.display = "none";
-//                alert("deshabilita preguntas");
-              break;
+                break;
             }
             else{
                pregunta2.style.display =  "";
@@ -112,7 +36,6 @@ function showPreguntasEstablecimientos(fieldset_main){
                bebidas_alimentos.style.display =  "";
                botanas_reposterias.style.display =  "";
                materiales.style.display =  "";
-//               alert("habilita preguntas");
                break;
                }
       }
@@ -133,7 +56,9 @@ function showUnicaPreguntaEstablecimientos(checked_object){
         if(checked_controller.type == 'checkbox' && checked_controller.checked && (checked_controller.value == "CLAE" || checked_controller.value == "CAEC" ))
              {
                 pregunta2.style.display = 'none';
+                pregunta2.disabled = true;
                 pregunta3.style.display = 'none';
+                pregunta3.disabled = true;
                 pregunta4.style.display = 'none';
                 preparacion_utensilios.style.display = "none";
                 higiene.style.display = "none";
@@ -141,7 +66,6 @@ function showUnicaPreguntaEstablecimientos(checked_object){
                 bebidas_alimentos.style.display = "none";
                 botanas_reposterias.style.display = "none";
                 materiales.style.display = "none";
-//                alert("deshabilita preguntas");
                }
          else{
                pregunta2.style.display =  "";
@@ -153,10 +77,17 @@ function showUnicaPreguntaEstablecimientos(checked_object){
                bebidas_alimentos.style.display =  "";
                botanas_reposterias.style.display =  "";
                materiales.style.display =  "";
-//               alert("habilita preguntas");
               }
         }
-      
+
+function showImageValue(comboSelect, divField,value){
+    var select = document.getElementById(comboSelect);
+    var div = document.getElementById(divField);
+    if(select.value !=  value)
+        div.style.display = 'block';
+    else
+        div.style.display = 'none';
+}
 
 
 /* Funciones usadas en EJE 5 */
