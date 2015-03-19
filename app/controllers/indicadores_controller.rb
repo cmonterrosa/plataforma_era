7c44f62a429934bf5de7590361f5515feb837360
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2015-03-18 20:43:24 -0600

    + Ajuste en indicadores

--- app/controllers/indicadores_controller.rb
+++ app/controllers/indicadores_controller.rb
@@ -7,6 +7,7 @@ class IndicadoresController < ApplicationController
   ##################################################
 
   def index
+    indicadores_generales
     indicadores_eje1
     indicadores_eje2
     indicadores_eje3
@@ -15,6 +16,7 @@ class IndicadoresController < ApplicationController
   end
 
   def update_indicadores
+    indicadores_generales
     indicadores_eje1
     indicadores_eje2
     indicadores_eje3
@@ -26,6 +28,26 @@ class IndicadoresController < ApplicationController
   ## Consulta de indicadores ###
 
 private
+
+  def indicadores_generales
+    ### Todas las que poseen cuentas de usuario ##
+    #@escuelas = Escuela.find(:all, :select => "e.id", :joins => "e, users u", :conditions => ["e.id = u.escuela_id"])
+    ### Solo las registradas ##3
+    @escuelas = Escuela.find(:all, :select => "id", :conditions => ["registro_completo = 1"])
+    @turnos = Escuela.find(:all, :select => "count(turno)", :conditions => ["turno IS NOT NULL"], :group => "turno")
+
+    ELECT species, COUNT(*) FROM pet GROUP BY species;
+
+
+    @escuelas_matutinas = Escuela.count(:id, :conditions => ["turno = ? AND id in (?)", "MATUTINO", @escuelas.map{|i|i.id}])
+    @escuelas_vespertinas = Escuela.count(:id, :conditions => ["turno = ? AND id in (?)", "VESPERTINO", @escuelas.map{|i|i.id}])
+
+    @alumnos = Escuela.sum(:alu_hom, :conditions => ["alu_hom IS NOT NULL AND id in (?)", @escuelas.map{|i|i.id}])
+    @alumnas = Escuela.sum(:alu_muj, :conditions => ["alu_muj IS NOT NULL AND id in (?)", @escuelas.map{|i|i.id}])
+    @docentes = Escuela.sum(:total_personal_docente, :conditions => ["total_personal_docente IS NOT NULL AND id in (?)", @escuelas.map{|i|i.id}])
+    @personal_admvo = Escuela.sum(:total_personal_admvo, :conditions => ["total_personal_admvo IS NOT NULL AND id in (?)", @escuelas.map{|i|i.id}])
+    @personal_apoyo = Escuela.sum(:total_personal_apoyo, :conditions => ["total_personal_apoyo IS NOT NULL AND id in (?)", @escuelas.map{|i|i.id}])
+  end
 
   def indicadores_eje1
     @docentes_capacitados_salud = Competencia.sum(:dctes_cap_salud, :conditions => ["dctes_cap_salud IS NOT NULL"])
