43e4188c641ea55aedce801afd5d63b30276a844
Rafael Hernández Escobar <rhernandez@snaktax.local>
2015-03-20 15:59:42 -0600

    Agrega eje 3 y eje 4 al reporte general con puntaje

--- app/controllers/diagnosticos_controller.rb
+++ app/controllers/diagnosticos_controller.rb
@@ -123,119 +123,54 @@ class DiagnosticosController < ApplicationController
     @e_maxptos = (($entorno_p2.to_f + $entorno_p3.to_f + $entorno_p5.to_f)*100).round(3)
     @e_totalptos = (@ep2.to_f + @ep3.to_f + @ep5.to_f).round(3)
     @e_porcentaje = @e_totalptos.to_f > 0 ? ((@e_totalptos.to_f * 100) / @e_maxptos.to_f).round(3) : 0
-#
-#    # -- Huella ---
-#    @huella = @diagnostico.huella if @diagnostico.huella
-#      # -- Operaciones --
-#      @hp1 = @huella.capacitacion_ahorro_energia.to_f > 0 ? (((@huella.capacitacion_ahorro_energia.to_f / 2 ) * 100) * $huella_p1.to_f).round(3) : 0
-#
-#      if @huella.consumo_anterior.to_f > 0 and @huella.consumo_actual.to_f > 0
-#        @huella.consumo_anterior.to_f > @huella.consumo_actual.to_f ? @hp2 = $huella_p2 * 100 : @hp2 = 0 if @huella.consumo_anterior or @huella.consumo_actual
-#      else
-#        @hp2 = 0
-#      end
-#
-#      @s_electricas = selected(@huella.energia_electrica) if @huella.energia_electrica
-#      @hp3 = @huella.focos_ahorradores.to_f > 0 ? (((@huella.focos_ahorradores.to_f / @huella.total_focos.to_f ) * 100) * $huella_p3.to_f).round(3) : 0
-#
-#      @s_aguas = selected(@huella.servicio_agua) if @huella.servicio_agua
-#      @huella.red_publica_agua == "SI" ? @hp4 = $huella_p4 * 100 : @hp4 = 0
-#      @hp5 = (((@huella.mantto_inst.to_f / 2 ) * 100) * $huella_p5.to_f).round(3)
-#
-#      @s_elimina_residuos = multiple_selected_id(@huella.elimina_residuos) if @huella.elimina_residuos
-#      @huella.recip_residuos_solid == "SI" ? @hp6 = $huella_p6 * 100 : @hp6 = 0
-#
-#      @huella.sep_residuos_org_inorg == "SI" ? @hp7 = $huella_p7 * 100 : @hp7 = 0
-#
-#      @huella.elabora_compostas == "SI" ? @hp8 = $huella_p8 * 100 : @hp8 = 0
-#
-#      @s_inorganicos = multiple_selected_id(@huella.inorganicos) if @huella.inorganicos
-#      (@s_inorganicos.size == 1 and @huella.inorganicos[0]['clave'] == "NING") ? @hp9 = 0 : @hp9 = ptos_inorganicos(@s_inorganicos.size)
-#
-#      @h_maxptos = (($huella_p1.to_f + $huella_p2.to_f + $huella_p3.to_f + $huella_p4.to_f + $huella_p5.to_f + $huella_p6.to_f + $huella_p7.to_f + $huella_p8.to_f + $huella_p9.to_f)*100).round(3)
-#      @h_totalptos = (@hp1.to_f + @hp2.to_f + @hp3.to_f + @hp4.to_f + @hp5.to_f + @hp6.to_f + @hp7.to_f + @hp8.to_f + @hp9.to_f).round(3)
-#      @h_porcentaje = @h_totalptos.to_f > 0 ? ((@h_totalptos.to_f * 100) / @h_maxptos.to_f).round(3) : 0
-#
-#    # -- Consumos --
-#    @consumo = @diagnostico.consumo if @diagnostico.consumo
-#      # -- Operaciones --
-#      if @escuela.nivel_descripcion == "BACHILLERATO"
-#        @establecimientos = Establecimiento.find(:all, :conditions => ["nivel not in ('BASICA')"])
-#      else
-#        @establecimientos = Establecimiento.find(:all)
-#      end
-#      @s_establecimientos = multiple_selected(@consumo.establecimientos) if @consumo.establecimientos
-#
-#      @consumo.conocen_lineamientos_grales == "SI" ? @cop2 = $consumo_p2 * 100 : @cop2 = 0
-#
-#      @consumo.capacitacion_alim_bebidas == "SI" ? @cop3 = $consumo_p3 * 100 : @cop3 = 0
-#
-#      @s_preparacions = multiple_selected(@consumo.preparacions) if @consumo.preparacions
-#      @s_utensilios = multiple_selected(@consumo.utensilios) if @consumo.utensilios
-#      @s_higienes = multiple_selected(@consumo.higienes) if @consumo.higienes
-#      @t_preparacions = Preparacion.all
-#      @t_utensilios = Utensilio.all
-#      @t_higiene = Higiene.all
-#
-#      @preparacions = @s_preparacions.size > 0 ? (((@s_preparacions.size.to_f / @t_preparacions.size.to_f) * 100) * $consumo_p4) : 0
-#      @utensilios = @s_utensilios.size > 0 ? (((@s_utensilios.size.to_f / @t_utensilios.size.to_f) * 100) * $consumo_p4) : 0
-#      @higienes = @s_higienes.size > 0 ? (((@s_higienes.size.to_f / @t_higiene.size.to_f) * 100) * $consumo_p4) : 0
-#
-#      @cop4 = (@s_preparacions.size + @s_utensilios.size + @s_higienes.size).to_f > 0 ? (@preparacions + @utensilios + @higienes).round(3) : 0
-#
-#      @s_bebidas = multiple_selected(@consumo.bebidas) if @consumo.bebidas
-#      @s_alimentos = multiple_selected(@consumo.alimentos) if @consumo.alimentos
-#      @s_botanas = multiple_selected(@consumo.botanas) if @consumo.botanas
-#      @s_reposterias = multiple_selected(@consumo.reposterias) if @consumo.reposterias
-#
-#      @b_saludables = Bebida.find_all_by_tipo("SALUDABLE")
-#      @select_bebidas = 0
-#      @s_bebidas.each do |bebida|
-#        @select_bebidas+=1 if @b_saludables.any? { |b| b[:clave] == bebida }
-#      end
-#      @bebidas = @select_bebidas.to_f > 0 ? (((@select_bebidas.to_f / @s_bebidas.size.to_f)* 100)* $consumo_p5).round(3) : 0
-#
-#      @a_saludables = Alimento.find_all_by_tipo("SALUDABLE")
-#      @select_alimentos = 0
-#      @s_alimentos.each do |alimento|
-#        @select_alimentos+=1 if @a_saludables.any? { |b| b[:clave] == alimento }
-#      end
-#      @alimentos = @select_alimentos.to_f > 0 ? (((@select_alimentos.to_f / @s_alimentos.size.to_f)* 100)* $consumo_p5).round(3) : 0
-#
-#      @bo_saludables = Botana.find_all_by_tipo("SALUDABLE")
-#      @select_botanas = 0
-#      @s_botanas.each do |botana|
-#        @select_botanas+=1 if @bo_saludables.any? { |b| b[:clave] == botana }
-#      end
-#      @botanas = @select_botanas.to_f > 0 ? (((@select_botanas.to_f / @s_botanas.size.to_f)* 100)* $consumo_p5).round(3) : 0
-#
-#      @r_saludables = Reposteria.find_all_by_tipo("SALUDABLE")
-#      @select_reposterias = 0
-#      @s_reposterias.each do |reposteria|
-#        @select_reposterias+=1 if @r_saludables.any? { |b| b[:clave] == reposteria }
-#      end
-#      @reposterias =  @select_reposterias.to_f > 0 ? (((@select_reposterias.to_f / @s_reposterias.size.to_f)* 100)* $consumo_p5).round(3) : 0
-#
-#      @cop5 = (@bebidas + @alimentos + @botanas + @reposterias).to_f > 0 ?  (@bebidas + @alimentos + @botanas + @reposterias) : 0
-#
-#      @s_materials = multiple_selected(@consumo.materials) if @consumo.materials
-#      @m_recomendables = Material.find_all_by_tipo("RECOMENDABLES")
-#      @select_materials = 0
-#      @s_materials.each do |material|
-#        @select_materials+=1 if @m_recomendables.any? { |b| b[:clave] == material }
-#      end
-#      @cop6 = (((@select_materials.to_f / @s_materials.size.to_f)* 100)* $consumo_p6).round(3)
-#
-#      @s_afisicas = selected(@consumo.frecuencia_afisica) if @consumo.frecuencia_afisica
-#      @cop7 = ptos_afisica(@s_afisicas)
-#
-#      minutos_activacion_fisica = (@consumo.minutos_activacion_fisica) ? @consumo.minutos_activacion_fisica : 0
-#      @cop8 = ptos_minutos(minutos_activacion_fisica > 30 ? 30 : @consumo.minutos_activacion_fisica)
-#
-#      @co_maxptos = (($consumo_p2.to_f + $consumo_p3.to_f + ($consumo_p4.to_f * 3) + ($consumo_p5.to_f * 4) + $consumo_p6.to_f + $consumo_p7.to_f + $consumo_p8.to_f)*100 ).round(3)
-#      @co_totalptos = (@cop2.to_f + @cop3.to_f + @cop4.to_f + @cop5.to_f + @cop6.to_f + @cop7.to_f + @cop8.to_f).round(3)
-#      @co_porcentaje = @co_totalptos.to_f > 0 ? ((@co_totalptos.to_f * 100) / @co_maxptos.to_f).round(3) : 0
-#
+
+    # -- Huella ---
+    @huella = @diagnostico.huella if @diagnostico.huella
+    @s_electricas = selected(@huella.energia_electrica) if @huella.energia_electrica
+    @s_inorganicos = multiple_selected_id(@huella.inorganicos) if @huella.inorganicos
+    @s_servicio_aguas = multiple_selected_id(@huella.servicio_aguas) if @huella.servicio_aguas
+    @s_elimina_residuos = multiple_selected_id(@huella.elimina_residuos) if @huella.elimina_residuos
+    @focos = 0..499
+    @hp1= diagnostico.puntaje_eje3_p1
+    @hp3= diagnostico.puntaje_eje3_p3
+    @hp5= diagnostico.puntaje_eje3_p5
+    @hp7= diagnostico.puntaje_eje3_p7
+    @hp8= diagnostico.puntaje_eje3_p8
+    @hp9= diagnostico.puntaje_eje3_p9
+
+    @h_maxptos = (($huella_p1.to_f + $huella_p3.to_f + $huella_p5.to_f + $huella_p7.to_f + $huella_p8.to_f + $huella_p9.to_f)*100).round(3)
+    @h_totalptos = (@hp1.to_f + @hp3.to_f + @hp5.to_f + @hp7.to_f + @hp8.to_f + @hp9.to_f).round(3)
+    @h_porcentaje = @h_totalptos.to_f > 0 ? ((@h_totalptos.to_f * 100) / @h_maxptos.to_f).round(3) : 0
+
+    # -- Consumos --
+    @consumo = @diagnostico.consumo if @diagnostico.consumo
+    if @escuela.nivel_descripcion == "BACHILLERATO"
+      @establecimientos = Establecimiento.find(:all, :conditions => ["nivel not in ('BASICA')"])
+    else
+      @establecimientos = Establecimiento.find(:all)
+    end
+    @s_establecimientos = multiple_selected(@consumo.establecimientos) if @consumo.establecimientos
+    @s_preparacions = multiple_selected(@consumo.preparacions) if @consumo.preparacions
+    @s_utensilios = multiple_selected(@consumo.utensilios) if @consumo.utensilios
+    @s_higienes = multiple_selected(@consumo.higienes) if @consumo.higienes
+    @s_bebidas = multiple_selected(@consumo.bebidas) if @consumo.bebidas
+    @s_alimentos = multiple_selected(@consumo.alimentos) if @consumo.alimentos
+    @s_botanas = multiple_selected(@consumo.botanas) if @consumo.botanas
+    @s_reposterias = multiple_selected(@consumo.reposterias) if @consumo.reposterias
+    @s_materials = multiple_selected(@consumo.materials) if @consumo.materials
+    @s_afisicas = selected(@consumo.frecuencia_afisica) if @consumo.frecuencia_afisica
+    @cop2= diagnostico.puntaje_eje4_p2
+    @cop3= diagnostico.puntaje_eje4_p3
+    @cop4= diagnostico.puntaje_eje4_p4
+    @cop5= diagnostico.puntaje_eje4_p5
+    @cop6= diagnostico.puntaje_eje4_p6
+    @cop7= diagnostico.puntaje_eje4_p7
+    @cop8= diagnostico.puntaje_eje4_p8
+
+    @co_maxptos = (($consumo_p2.to_f + $consumo_p3.to_f + ($consumo_p4.to_f * 3) + ($consumo_p5.to_f * 4) + $consumo_p6.to_f + $consumo_p7.to_f + $consumo_p8.to_f)*100 ).round(3)
+    @co_totalptos = (@cop2.to_f + @cop3.to_f + @cop4.to_f + @cop5.to_f + @cop6.to_f + @cop7.to_f + @cop8.to_f).round(3)
+    @co_porcentaje = @co_totalptos.to_f > 0 ? ((@co_totalptos.to_f * 100) / @co_maxptos.to_f).round(3) : 0
+
 #      # -- Participación --
 #      @participacion = @diagnostico.participacion if @diagnostico.participacion
 #      # -- Operaciones --
