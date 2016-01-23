5264255cd031268282bfc939f536451bcf3a1e11
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2016-01-22 18:54:38 -0600

    + Ajuste en migrate

--- db/migrate/177_truncate_usuarios.rb
+++ db/migrate/177_truncate_usuarios.rb
@@ -12,86 +12,49 @@ class TruncateUsuarios < ActiveRecord::Migration
     execute("update escuelas set evidencias_internet = NULL;")
     execute("delete from roles_users where user_id NOT IN (select id from users);")
     execute("update escuelas set estatu_id = NULL;")
-    execute("
-      truncate actividads;
-
-      truncate adjuntos;
-
-      truncate alumnos_capacitados;
-
-      truncate antecedentes;
-
-      truncate avances;
-
-      truncate bitacoras;
-
-      truncate capacitacion_padres;
-
-      truncate comentarios;
-
-      truncate competencias;
-
-      truncate consumos;
-
-      truncate consumos_alimentos;
-
-      truncate consumos_bebidas;
-
-      truncate consumos_botanas;
-
-      truncate consumos_establecimientos;
-
-      truncate consumos_higienes;
-
-      truncate consumos_materials;
-
-      truncate consumos_preparacions;
-
-      truncate consumos_reposterias;
-
-      truncate consumos_utensilios;
-
-      truncate cortes;
-
-      truncate diagnosticos;
-
-      truncate docentes_capacitados;
-
-      truncate ejes;
-
-      truncate entornos;
-
-      truncate entornos_acciones;
-
-      truncate escuelas_espacios;
-
-      truncate escuelas_generacions;
-
-      truncate escuelas_programas;
-
-      truncate escuelas_users;
-
-      truncate evaluacions;
-
-      truncate huellas;
-
-      truncate huellas_elimina_residuos;
-
-      truncate huellas_inorganicos;
-
-      truncate huellas_servicio_aguas;
-
-      truncate mensajes;
-
-      truncate nivel_certificacions;
-
-      truncate participacions;
-
-      truncate pescolars;
-
-      truncate proyectos;
-
-      truncate ranking_historicos;")
+    @truncates= "truncate actividads;
+truncate adjuntos;
+truncate alumnos_capacitados;
+truncate antecedentes;
+truncate avances;
+truncate bitacoras;
+truncate capacitacion_padres;
+truncate comentarios;
+truncate competencias;
+truncate consumos;
+truncate consumos_alimentos;
+truncate consumos_bebidas;
+truncate consumos_botanas;
+truncate consumos_establecimientos;
+truncate consumos_higienes;
+truncate consumos_materials;
+truncate consumos_preparacions;
+truncate consumos_reposterias;
+truncate consumos_utensilios;
+truncate cortes;
+truncate diagnosticos;
+truncate docentes_capacitados;
+truncate ejes;
+truncate entornos;
+truncate entornos_acciones;
+truncate escuelas_espacios;
+truncate escuelas_generacions;
+truncate escuelas_programas;
+truncate escuelas_users;
+truncate evaluacions;
+truncate huellas;
+truncate huellas_elimina_residuos;
+truncate huellas_inorganicos;
+truncate huellas_servicio_aguas;
+truncate mensajes;
+truncate nivel_certificacions;
+truncate participacions;
+truncate pescolars;
+truncate proyectos;
+truncate ranking_historicos;"
+@truncates.each do |t|
+          execute(t.strip)
+  end
   end
 
   def self.down
